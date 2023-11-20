80ebf4850883a55a1cd7c1de78d74175088af9bc
overthecloud96 <151497223+overthecloud96@users.noreply.github.com>
2023-11-20 16:30:06 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
-touch /root/cicd/hellonode/123456
+current_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "현재 시간: $current_time"
+file_path="/root/cicd/hellonode/123456"
+echo "$current_time" > "$file_path"
+echo "파일 작성이 완료되었습니다: $file_path"
