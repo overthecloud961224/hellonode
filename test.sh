a4dd0396c285407c01baa289e175d93d0cfc25a2
overthecloud96 <151497223+overthecloud96@users.noreply.github.com>
2023-11-20 16:32:51 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 current_time=$(date +"%Y-%m-%d %H:%M:%S")
 echo "time: $current_time"
 file_path="/root/cicd/hellonode/123456.txt"
-echo "$current_time" > "$file_path"
+echo "$current_time" >> "$file_path"
 echo "done: $file_path"
